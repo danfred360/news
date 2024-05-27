9903b2f07e97061e39acd54844fb378ef6b3058b
Daniel Frederick <dannyfrederick360@gmail.com>
2024-05-27 18:36:38 +0000

    chore: initial python app

---
+++ src/config.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+from typing import Final, final
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+@lru_cache()
+def get_config():
+    """Get the configuration for the application. This is cached for performance."""
+    return Config()
+
+
+@final
+class Config(BaseSettings):
+    app_name: str = "blogger"
+    news_api_key: Final[str]
+
+    model_config = SettingsConfigDict(env_file=".env")
